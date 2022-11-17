e86ce41c48277f627639afb9acab7cf11b2ced6f
Venyamin Shvets <venyashvets@gmail.com>
2022-11-17 12:27:50 +0300

    feat: add pagination for queue command

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -2,14 +2,21 @@ import { messageCreators } from '../messages';
 import { CommandHandler } from '../types/command';
 import { handleConnectionCreation } from '../utils';
 
-export const queueCommandRegExp = /^!(queue|q) *$/g;
+export const queueCommandRegExp = /^!(queue|q) */g;
 
 export const queueCommandHandler: CommandHandler = handleConnectionCreation(
   (msg, connection) => {
+    const arg = msg.content.replace(queueCommandRegExp, '');
+    const page = arg.length === 0 ? 0 : +arg - 1;
+    if (Number.isNaN(page)) {
+      connection.sendMessage('Page must be number');
+      return;
+    }
+
     const tracks = connection.player.getQueue();
     connection.sendMessage(
       tracks.length
-        ? messageCreators.queue(tracks)
+        ? messageCreators.queue(tracks, page)
         : messageCreators.queueEmpty(),
     );
   },
--- src/messages.ts
+++ src/messages.ts
@@ -7,10 +7,15 @@ export const messageCreators = {
   pause: (trackTitle: string) => `*Track* **| ${trackTitle} |** *paused*`,
   unpause: (trackTitle: string) => `*Track* **| ${trackTitle} |** *unpaused*`,
   addedToQueue: (name: string) => `**| ${name} |** *added to queue*`,
-  queue: (tracks: Track[]) =>
-    `\n*Queue*\n${tracks
-      .map((track, i) => `*${i + 1}.* **${track.getTitle()}**`)
-      .join('\n')}`,
+  queue: (tracks: Track[], page: number) => {
+    const itemsPerPage = 10;
+    const pages = Math.ceil(tracks.length / itemsPerPage);
+    const startIndex = page * itemsPerPage;
+    return `\n*Queue*\n${tracks
+      .slice(startIndex, startIndex + itemsPerPage)
+      .map((track, i) => `*${startIndex + i + 1}.* **${track.getTitle()}**`)
+      .join('\n')}\n**Page** \`${page + 1}\` of \`${pages}\``;
+  },
   queueEmpty: () => `**Queue** *is empty*`,
   leave: (channelName: string) =>
     `*leaving channel **${channelName}**, I don't need **you** anyway*`,
